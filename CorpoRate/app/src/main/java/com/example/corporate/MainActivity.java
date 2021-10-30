25fbf3f6cee33551ea1fa52827617d7fd53ce3b7
ppaul895 <ppaul895@gmail.com>
2021-10-30 00:44:19 -0400

    new skeleton home and search pages

--- CorpoRate/app/src/main/java/com/example/corporate/MainActivity.java
+++ CorpoRate/app/src/main/java/com/example/corporate/MainActivity.java
@@ -1,31 +1,25 @@
 package com.example.corporate;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.SearchView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private SearchView search;
-
+    public static final String EXTRA_MESSAGE = "com.example.corporate.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        search = findViewById(R.id.search);
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                openSearchResults();
-
-            }
-        });
     }
-    public void openSearchResults(){
+
+    /** Called when the user taps the Search button */
+    public void openSearchResults(View view) {
         Intent intent = new Intent(this, searchActivity.class);
+        EditText searchField = (EditText) findViewById(R.id.searchField);
+        String searchQuery = searchField.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, searchQuery);
         startActivity(intent);
     }
 }
