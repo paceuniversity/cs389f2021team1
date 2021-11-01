48c20f9aaf6ad668af8e68ef1ab37265837250a5
ppaul895 <ppaul895@gmail.com>
2021-11-01 13:07:17 -0400

    set Profile title and added Home page to topbar navigation

--- CorpoRate/app/src/main/java/com/example/corporate/ProfileActivity.java
+++ CorpoRate/app/src/main/java/com/example/corporate/ProfileActivity.java
@@ -2,9 +2,6 @@ package com.example.corporate;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
