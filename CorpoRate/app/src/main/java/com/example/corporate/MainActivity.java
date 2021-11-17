ab4ddac22e56921e8237308d45e31dad87e2f839
geetanjali <gk72727p@pace.edu>
2021-11-16 23:09:49 -0500

    making changes to home page

--- CorpoRate/app/src/main/java/com/example/corporate/MainActivity.java
+++ CorpoRate/app/src/main/java/com/example/corporate/MainActivity.java
@@ -1,29 +1,58 @@
 package com.example.corporate;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private Toolbar toolbar;
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private TextView username1;
+    private TextInputEditText name, username, email, password;
+    private TextView headerName, headerUsername, numofReviews, numOfReviewsLabel;
+    private SwitchCompat anonymousSwitch;
+    private static final String TAG = "mainActivity";
+
     public static final String EXTRA_MESSAGE = "com.example.corporate.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setTitle("Home");
@@ -40,7 +69,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setCheckedItem(R.id.nav_home);
+
+
+        username1 = findViewById(R.id.displayName);
+
+
+        DocumentReference docRef = db.collection("Users")
+                .document((Objects.requireNonNull(auth.getCurrentUser()).getUid()));
+
+
+        String name = auth.getCurrentUser().getEmail();
+        //String name = Objects.requireNonNull(auth.getCurrentUser()).getUid().toString();
+        username1.setText(name);
+
+
+
+
     }
+
 
     /** Called when the user taps the Search button */
     public void openSearchResults(View view) {
